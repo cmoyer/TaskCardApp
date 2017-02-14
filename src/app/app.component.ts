8cbd1069e87bb939338ec5ac7c2daea96e2e11da
Chad Moyer <chad.moyer@trumbullcorp.com>
2017-02-14 15:31:58 -0500

    Added a small form with 2 way binding to use when we want to add tasks that aren't in a predefined datasource in the code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Task } from './model/task';
+
 @Component({
-  moduleId: module.id,
-  selector: 'my-app',
-  templateUrl: 'app.component.html'
+    moduleId: module.id,
+    selector: 'my-app',
+    templateUrl: 'app.component.html',
+    styleUrls: [ 'app.component.css' ]
 
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {
+    private tasks = [
+        new Task("Buy a monkey", false),
+        new Task("Walk the turtle", false)
+    ];
+    private currentTask = new Task(null, false);
+}
